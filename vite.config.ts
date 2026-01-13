b0a66743aa12aa009aba8c00fdfdd3f8c8ee4087
Gabriela dos Santos <65128340+gabisinhas@users.noreply.github.com>
2026-01-13 12:11:27 -0300

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/frontend-insightglobal-application/',
+  base: './',
   publicDir: 'public',
   plugins: [react()],
 });
